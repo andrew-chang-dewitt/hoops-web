eff39543924d277ff344bf877f6753674e1fe009
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2022-01-15 21:17:09 -0500

    Add registration pages.

---
+++ src/routes/register/index.svelte
@@ -0,0 +1,50 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit'
+
+  export const load: Load = async ({session}) => 
+    session.token ? {status: 302, redirect: '/'} : {}
+</script>
+
+<script lang="ts">
+  import {goto} from '$app/navigation'
+  import {session} from '$app/stores'
+  import {isFetchError, FetchError, User} from '$lib/api'
+
+  import Input from '$lib/components/Input.svelte'
+
+  let handle = ''
+  let password = ''
+  let fullName = ''
+  let preferredName = ''
+
+  let error: FetchError
+
+  const submit = async () => {
+    const response = await User.post({
+      data: {handle, password, full_name: fullName, preferred_name: preferredName},
+      token: $session.token,
+    })
+
+    if (isFetchError(response)) {
+      error = response
+    }
+    else {
+      goto(`/register/welcome-${preferredName ? preferredName : fullName}`)
+    }
+  }
+</script>
+
+<div>
+  {#if error}
+    <p>{error.message}</p>
+  {/if}
+
+  <form on:submit|preventDefault={submit}>
+    <Input name="preferredName" label="Preferred Name" bind:value={preferredName} />
+    <Input name="fullName" label="Full Name" bind:value={fullName} />
+    <Input name="handle" label="Handle" bind:value={handle} />
+    <Input name="password" inputType="password" label="Password" bind:value={password} />
+
+    <button type="submit">Submit</button>
+  </form>
+</div>
---
+++ src/routes/register/welcome-[name].svelte
@@ -0,0 +1,24 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit'
+
+  let userName: string = ''
+
+  export const load: Load = async ({session, params}) => {
+    userName = params.name
+
+    return session.token ? {status: 302, redirect: '/'} : {}
+  }
+</script>
+
+<svelte:head>
+  <title>Hoops | Welcome, {userName}!</title>
+</svelte:head>
+
+<h2>Welcome, {userName}!</h2>
+
+<p>
+  I hope you'll enjoy saving money using Hoops as much as I've enjoyed creating 
+  it! <a href="/login">Log in</a> with your new username & password to get 
+  started, and please don't hesitate to let me know below if there's anything 
+  that would make Hoops better for you.
+</p>
